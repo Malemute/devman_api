ff1d6a3ce810beaea590f494bc588acae013d757
Malemute <eugeny.mamontov@gmail.com>
2020-03-03 22:35:01 +0300

    Update bitly_getprofile.py

--- bitly_getprofile.py
+++ bitly_getprofile.py
@@ -1,6 +1,6 @@
 import requests
 
-request_headers = {"Authorization":"Bearer c65b488a8ca81706b68d41030fd118461b056012"}
+request_headers = {"Authorization":"Bearer <your token here>"}
 url = 'https://api-ssl.bitly.com/v4/user'
 response = requests.get(url, headers = request_headers)
 response.raise_for_status()
