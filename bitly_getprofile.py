fc98805e5169b46a67e23370f99e0f021262f6e1
Malemute <eugeny.mamontov@gmail.com>
2020-03-04 15:30:00 +0300

    Delete bitly_getprofile.py

--- bitly_getprofile.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-request_headers = {"Authorization":"Bearer <your token here>"}
-url = 'https://api-ssl.bitly.com/v4/user'
-response = requests.get(url, headers = request_headers)
-response.raise_for_status()
-print(response.json())
-
